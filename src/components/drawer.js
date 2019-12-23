010ecf15568793bb28d938dc57bc4c36f8cf3a6d
MatviyO <33325458+MatviyO@users.noreply.github.com>
2019-12-23 18:51:20 +0200

    add1231

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import MenuItem from '@material-ui/core/MenuItem';
-
+import SvgIcon from '@material-ui/core/SvgIcon';
 
 export const AppDrawer = (props) => {
 
@@ -12,7 +12,9 @@ export const AppDrawer = (props) => {
 
 
                     >
-            <MenuItem> Home</MenuItem>
+            <MenuItem><SvgIcon {...props}>
+                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+            </SvgIcon> Home</MenuItem>
             <MenuItem>Contact</MenuItem>
         </Drawer>
     )
